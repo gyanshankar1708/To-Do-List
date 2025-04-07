9859c96b65b7778ca39ca29fb250faa16f993f8f
Gyanshankar <gyanshankar1708@gmail.com>
2025-04-07 23:55:18 +0530

    Project Created

---
+++ script.js
@@ -0,0 +1,35 @@
+let btn = document.getElementById("btn");
+let list = document.querySelector(".list");
+let clrbtn = document.getElementById("clear");
+
+
+const htmlCreate = (str) => {
+  return `<div class="items">${str}</div>`;
+};
+
+
+function localStore(note){
+  let notes = JSON.parse(localStorage.getItem("Do")) || [];
+  notes.push(note);
+  localStorage.setItem("Do", JSON.stringify(notes));
+}
+
+window.onload = () => {
+  let toDo = JSON.parse(localStorage.getItem("Do")) || [];
+  toDo.forEach(item => {
+      list.innerHTML += htmlCreate(item);
+  });
+};
+
+btn.addEventListener("click", () => {
+  let val = document.getElementById("note").value;
+  list.innerHTML += htmlCreate(val);
+  localStore(val);
+  document.getElementById("note").value = "";
+});
+
+
+clrbtn.addEventListener("click",()=>{
+  localStorage.removeItem("Do");
+  list.innerHTML = "";
+});
